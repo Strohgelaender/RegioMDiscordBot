c126e1d2fbf66997e8604b01c7b5d28d07d40d29
Lucas <ga53foy@mytum.de>
2021-01-12 02:10:43 +0100

    added role ronstants

--- src/main/java/de/fll/regiom/model/Constants.java
+++ src/main/java/de/fll/regiom/model/Constants.java
@@ -7,6 +7,10 @@ public abstract class Constants {
 
 	public static final long GUILD_ID = 788772759756996681L;
 
+	public static final long TEAMAREA_CATEGORY_ID = 795645281625571399L;
+
+	public static final long ADMIN_ROLE_ID = 795615662264287242L;
+	public static final long VOLUNTEER_ROLE_ID = 795616244317159424L;
 	public static final long TEAM_ROLE_ID = 795634839381344256L;
 	public static final long SPECTATOR_ROLE_ID = 795641201927651329L;
 }
