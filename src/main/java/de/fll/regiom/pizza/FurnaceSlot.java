8df510dd5ad8b0619037f1033492409cb3dde31b
rigob <ge35sot@mytum.de>
2021-01-12 14:55:16 +0100

    Added some more documentation for the pizza minigame

--- src/main/java/de/fll/regiom/pizza/FurnaceSlot.java
+++ src/main/java/de/fll/regiom/pizza/FurnaceSlot.java
@@ -3,7 +3,16 @@ package de.fll.regiom.pizza;
 public class FurnaceSlot {
 	private Pizza pizza;
 
+	/**
+	 * Fills the pizza into the slot and bakes it. The slot will be locked while this is happening.
+	 *
+	 * @param pizza The pizza which should be baked
+	 **/
 	public void bakePizza(Pizza pizza) {
+		if (this.pizza != null) {
+			System.out.println("An illegal attempt to bake a pizza has happened!");
+			return;
+		}
 		this.pizza = pizza;
 		new Thread(() -> {
 			try {
@@ -18,5 +27,9 @@ public class FurnaceSlot {
 
 	public Pizza getPizza() {
 		return pizza;
+	}
+
+	public boolean isFull() {
+		return pizza != null;
 	}
 }
