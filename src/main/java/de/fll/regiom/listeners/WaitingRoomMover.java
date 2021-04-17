5b6afdb701daa5a1dd92eff2ac3e80be6bf70403
rigob <ge35sot@mytum.de>
2021-04-17 19:54:53 +0200

    some weird beta implementation of name modification system for keeping camera image orders when users are joining

--- src/main/java/de/fll/regiom/listeners/WaitingRoomMover.java
+++ src/main/java/de/fll/regiom/listeners/WaitingRoomMover.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -16,12 +17,25 @@ public class WaitingRoomMover extends ListenerAdapter {
 			Map.of(830514879919685652L, 830514021516967976L, 831098421351022603L, 795634259091390504L);
 	private static final Map<Long, Boolean> MUTE_MAP = Map.of(830514879919685652L, true, 831098421351022603L, false);
 
+	private int counter = 0;
+	private final Map<Long, Integer> namePrefix = new HashMap<>();
+	private final Map<Long, String> removedNamePart = new HashMap<>();
+
 	@Override
-	public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
+	public synchronized void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
 		System.out.println("Update: " + event);
 		if (event.getChannelJoined() == null)
 			return;
 		long joinID = event.getChannelJoined().getIdLong();
+		if (joinID == 795608993627242506L) {
+			long entityID = event.getEntity().getIdLong();
+			namePrefix.computeIfAbsent(entityID, l -> counter++);
+			String name = event.getEntity().getEffectiveName();
+			name = namePrefix.get(entityID) + " " + name;
+			event.getEntity().getGuild().modifyNickname(event.getEntity(), name.substring(0, Math.min(31, name.length()))).queue();
+			if (name.length() > 32)
+				removedNamePart.put(entityID, name.substring(32));
+		}
 		if (WAITING_ROOM_MAP.containsKey(joinID)) {
 			moveToRoom(event.getEntity(), WAITING_ROOM_MAP.get(joinID));
 			return;
@@ -31,6 +45,13 @@ public class WaitingRoomMover extends ListenerAdapter {
 			return;
 		}
 		long leaveID = event.getChannelLeft().getIdLong();
+		if (leaveID == 795608993627242506L) {
+			long userID = event.getEntity().getIdLong();
+			String name = event.getEntity().getEffectiveName();
+			name = name.replaceFirst(namePrefix.get(userID) + " ", "");
+			name += removedNamePart.getOrDefault(userID, "");
+			event.getEntity().getGuild().modifyNickname(event.getEntity(), name).queue();
+		}
 		if (WAITING_ROOM_MAP.containsValue(joinID) && WAITING_ROOM_MAP.containsKey(leaveID)
 				&& WAITING_ROOM_MAP.get(leaveID) == joinID) {
 			if (RoleHelper.isTeam(event.getEntity()))
